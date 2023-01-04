d1b4656d2637565a2796682d9529c1c278a9eb73
BJ Bae <01bbae@gmail.com>
2023-01-03 18:59:20 -0800

    First Commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>Enhanced Youtube Search</header>
+      <form>
+        <input type="text" placeholder="Search Phrases"></input>
+        <input type="submit" value="Search"></input>
+      </form>
+    </div>
+  );
+}
+
+export default App;
